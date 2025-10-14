e8f7f99d8f5332821cfbeb852ba84cdfe2b1ce70
Anurag Birendra Singh <anuragbirendrasingh@gmail.com>
2025-10-14 16:05:18 +0530

    bubble sort

---
+++ Sorting/bubble_Sort.java
@@ -0,0 +1,23 @@
+package Sorting;
+
+public class bubble_Sort {
+    public static void main(String[] args) {
+        int[] arr = { 10, 22, 60, 40, 30, 10, 20 };
+        sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j + 1] < arr[j]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
