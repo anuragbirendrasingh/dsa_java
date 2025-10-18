2e96217ff05b69cdb00f856c44544a31f4d46004
Anurag Birendra Singh <anuragbirendrasingh@gmail.com>
2025-10-18 19:16:49 +0530

    print all substring length wise

---
+++ String/Substrings/FixedLength/printing_lengthWise.java
@@ -0,0 +1,17 @@
+package String.Substrings.FixedLength;
+
+public class printing_lengthWise {
+    public static void main(String[] args) {
+        String str = "ABC";
+
+        for(int len = 1 ; len<=str.length();len++){
+
+            for(int j = len ; j<=str.length() ; j++){
+
+                int i = j - len ;
+
+                System.out.println(str.substring(i, j));
+            }
+        }
+    }
+}
